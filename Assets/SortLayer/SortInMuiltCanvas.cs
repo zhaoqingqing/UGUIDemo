25c1690662d8bd9dc2695cd10ae37d27e86223a1
qingqing-zhao <zhaoqignqing@gmail.com>
2016-10-26 18:44:16 +0800

    测试 多个Canvas排序

---
+++ Assets/SortLayer/SortInMuiltCanvas.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 只有1个canvas的排序
+/// </summary>
+public class SortInMuiltCanvas : MonoBehaviour
+{
+
+    //http://blog.csdn.net/akailee/article/details/51798288 会增加Canvas
+    //http://forum.china.unity3d.com/thread-1572-1-1.html 修改SortLayer
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
