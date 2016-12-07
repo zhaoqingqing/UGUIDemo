2f042b5a8c7a7733505d41f06e681c07b3953cc0
qingqing-zhao <zhaoqignqing@gmail.com>
2016-12-07 15:33:24 +0800

    完善sort demo

---
+++ Assets/SortLayer/README.md
@@ -0,0 +1,11 @@
+
+
+### SortIn1Canvas
+
+同一个canvas下多个Rect的排序，使用GetSiblingIndex
+
+SetSiblingIndex(xxRect) 设置在xxRect的后面
+
+### SortInMuiltCanvas
+
+多个canvas的排序，使用SortOrder(整形)
--- Assets/SortLayer/SortInMuiltCanvas.cs
+++ Assets/SortLayer/SortInMuiltCanvas.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /// <summary>
-/// 只有1个canvas的排序
+/// 只有多个canvas的排序
+/// 如果设置Canvas的Sort Order属性,只能设置int值
 /// </summary>
 public class SortInMuiltCanvas : MonoBehaviour
 {
+    public Button BtnSort;
+    public Canvas CanvasWhite;
+    public Canvas CanvasRed;
+    public Canvas CanvasBlue;
 
     //http://blog.csdn.net/akailee/article/details/51798288 会增加Canvas
     //http://forum.china.unity3d.com/thread-1572-1-1.html 修改SortLayer
+
     // Use this for initialization
     void Start()
     {
-
+        BtnSort.onClick.AddListener(OnSort);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnSort()
     {
-
+        CanvasBlue.sortingOrder = -1;
     }
 }
