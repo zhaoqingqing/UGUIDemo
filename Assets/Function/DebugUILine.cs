35ed534faa7293832ee2aa005c25a994480f55bd
qingqing-zhao <zhaoqignqing@gmail.com>
2017-02-20 15:50:38 +0800

    add 检查raycast

---
+++ Assets/Function/DebugUILine.cs
@@ -0,0 +1,26 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//form http://www.xuanyusong.com/archives/4291
+//查看UI元素是否开启raycastTarget
+public class DebugUILine : MonoBehaviour {
+	static Vector3[] fourCorners = new Vector3[4];
+	void OnDrawGizmos()
+	{
+		foreach (MaskableGraphic g in GameObject.FindObjectsOfType<MaskableGraphic>())
+		{
+			if (g.raycastTarget)
+			{
+				RectTransform rectTransform = g.transform as RectTransform;
+				rectTransform.GetWorldCorners(fourCorners);
+				Gizmos.color = Color.blue;
+				for (int i = 0; i < 4; i++)
+					Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+ 
+			}
+		}
+	}
+}
+#endif
